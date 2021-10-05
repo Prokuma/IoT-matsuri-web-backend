b061f41cea1a17d9f07460d03dc1d5ca3c5dd436
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2021-10-05 21:42:40 +0900

    [add] #2 /device/* の実装。redisを使ったIoT側との連携、テストは未実装。

--- cruds/devices/__init__.py
+++ cruds/devices/__init__.py
@@ -1,34 +1,121 @@
-from db.models import Device, Message
+from typing import List
+from db import models 
+from fastapi.exceptions import HTTPException
 from sqlalchemy.orm.session import Session
+from schemas.device import Device, DeviceList, DeviceCreateResponse, DeviceStatus, DeviceMessageResponse
+from uuid import uuid4
 
-def get_device(db: Session, device_id: str) -> Device:
-    device = db.query(Device).filter(Device.id == device_id).first()
+def generate_uuid():
+    return str(uuid4())
 
-    return device
+# def get_device(db: Session, device_id: str) -> Device:
+#     device = db.query(Device).filter(Device.id == device_id).first()
 
-def get_devices_from_user_id(db: Session, user_id: str) -> list[Device]:
-    devices = db.query(Device).filter(Device.id == user_id)
+#     return device
+
+def get_devices_from_user_id(db: Session, user_id: str) -> List[Device]:
+    db_devices = db.query(models.Device).filter(models.Device.user_id == user_id).all()
+    if db_devices == None:
+        raise HTTPException(400, 'UserId does not exit')
+    devices=[]
+    for db_device in db_devices:
+        device = Device(
+            id = db_device.id,
+            secret = "",
+            name = db_device.name,
+            connection = False
+        )
+        devices.append(device)
 
     return devices
 
-def create_device(db: Session, device: Device) -> Device:
-    db.add(device)
+def get_device_from_id(db: Session, device_id: str, user_id: int) -> Device:
+    db_device = db.query(models.Device).filter(models.Device.user_id == user_id, models.Device.id == device_id).first()
+    if db_device == None:
+        raise HTTPException(400, 'Device does not exit')
+    
+    device = Device(
+        name = db_device.name,
+        id = db_device.id,
+        secret = db_device.secret,
+        connection = False
+    )
+
+    return device
+
+def change_device_info(db: Session, device_id: str, user_id: int, name: str) -> DeviceCreateResponse:
+    device_orm = db.query(models.Device).filter(models.Device.user_id == user_id, models.Device.id == device_id).first()
+    if device_orm == None:
+        raise HTTPException(400, 'Device does not exit')
+
+    device_orm.name = name
+
     db.commit()
+    db.refresh(device_orm)
+    device = DeviceCreateResponse.from_orm(device_orm)
+    return device
 
-    return get_device(db, device.id)
+def create_device(db: Session, name: str, user: models.LoginToken) -> DeviceCreateResponse:
 
-def get_message(db: Session, message_id: str) -> Message:
-    message = db.query(Message).filter(Message.id == message_id).first()
+    device_orm = models.Device(
+        user_id = user.user_id,
+        name = name,
+        secret = generate_uuid()
+    )
+
+    db.add(device_orm)
+    db.commit()
+    db.refresh(device_orm)
+    device = DeviceCreateResponse.from_orm(device_orm)
+    return device
+
+def delete_device(db: Session, device_id: str, user_id: int) -> str:
+    device_orm = db.query(models.Device).filter(models.Device.user_id == user_id, models.Device.id == device_id).first()
+    if device_orm == None:
+        raise HTTPException(400, 'Device does not exit')
+    db.delete(device_orm)
+    db.commit()
+    return "OK"
+
+def get_message(db: Session, message_id: str) -> models.Message:
+    message = db.query(models.Message).filter(models.Message.id == message_id).first()
 
     return message
 
-def get_messages_from_device_id(db: Session, device_id: str) -> list[Message]:
-    messages = db.query(Message).filter(Message.device_id == device_id)
+def get_messages_from_device_id(db: Session, device_id: str) -> List[DeviceMessageResponse]:
+    db_device = db.query(models.Device).filter(models.Device.id == device_id).first()
+    if db_device == None:
+        raise HTTPException(400, 'Device does not exit')
 
+    db_messages = db.query(models.Message).filter(models.Message.device_id == device_id).all()
+    if db_messages == []:
+        raise HTTPException(400, 'Message does not exit')
+
+    messages = []
+
+    for db_message in db_messages:
+        message = DeviceMessageResponse.from_orm(db_message)
+        messages.append(message)
     return messages
 
-def create_message(db: Session, message: Message) -> Message:
-    db.add(message)
-    db.commit()
+def create_message(message: models.Message, db: Session, device_id: str, user_id: int) -> DeviceMessageResponse:
+    db_device = db.query(models.Device).filter(models.Device.user_id == user_id, models.Device.id == device_id).first()
+    if db_device == None:
+        raise HTTPException(400, 'Device does not exit')
 
-    return get_message(db, message.id)
+    db_message = db.query(models.Message).filter(models.Message.device_id == device_id, models.Message.message == message).first()
+    if db_message != None:
+        raise HTTPException(400, 'Message exit')
+    
+    message_orm = models.Message(
+        is_to_device = False,
+        device_id = device_id,
+        message = message
+    )
+
+    db.add(message_orm)
+    db.commit()
+    db.refresh(message_orm)
+    message = DeviceMessageResponse.from_orm(message_orm)
+    print(message)
+    return message
--- routers/devices/__init__.py
+++ routers/devices/__init__.py
@@ -1,28 +1,56 @@
+from typing import List
+from schemas.user import User
 from fastapi import APIRouter
-from schemas.device import Device, DeviceCreateRequest, DeviceInformation, DeviceList, DeviceMessageRequest, DeviceStatus
+from fastapi.params import Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm.session import Session
+from schemas.device import Device, DeviceCreateRequest, DeviceCreateResponse ,DeviceInformation, DeviceList, DeviceMessageRequest, DeviceStatus, DeviceMessageResponse
+from db import get_db, models
+from cruds.users import create_user, get_user_from_token, get_password_hash, get_user
+from cruds.devices import get_devices_from_user_id, create_device, get_device_from_id, change_device_info, delete_device, create_message, get_messages_from_device_id
+import datetime
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 
 device_router = APIRouter()
 
-@device_router.get("/", response_model=DeviceList)
-async def devices_list():
-    devices = DeviceList()
+@device_router.get("/", response_model=List[Device])
+async def devices_list(db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    devices = get_devices_from_user_id(db, user.user_id)
     return devices
 
-@device_router.post("/register", response_model=DeviceCreateRequest)
-async def register(device: Device):
+@device_router.post("/register", response_model=DeviceCreateResponse)
+async def register(payload: DeviceCreateRequest, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    device = create_device(db, payload.name, user)
     return device
 
-@device_router.get("/device/{deivce_id}", response_model=DeviceInformation)
-async def get_device(device_id: str):
-    information = DeviceInformation()
-    return information
+@device_router.get("/device/{deivce_id}", response_model=Device)
+async def get_device(device_id: str, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    device = get_device_from_id(db, device_id, user.user_id)
+    return device
 
-@device_router.post("/device/{deivce_id}", response_model=DeviceStatus)
-async def device_operation(status: DeviceStatus, device_id: str):
-    status = DeviceStatus()
+@device_router.put("/device/{deivce_id}", response_model=DeviceCreateResponse)
+async def rename_device(payload: DeviceCreateRequest, device_id: str, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    device = change_device_info(db, device_id, user.user_id, payload.name)
+    return device
+
+@device_router.delete("/device/{deivce_id}", response_model=str)
+async def remove_device(device_id: str, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    status = delete_device(db, device_id, user.user_id)
     return status
 
-@device_router.delete("/device/{deivce_id}", response_model=DeviceStatus)
-async def remove_device(device_id: str):
-    status = DeviceStatus()
-    return status
+@device_router.post("/device/{deivce_id}/message", response_model=DeviceMessageResponse)
+async def device_operation(payload: DeviceMessageRequest, device_id: str, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    user = get_user_from_token(db, cred.credentials)
+    message = create_message(payload.message, db, device_id, user.user_id)
+    return message
+
+@device_router.get("/device/{deivce_id}/message", response_model=List[DeviceMessageResponse])
+async def get_device(device_id: str, db: Session = Depends(get_db), cred: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
+    messages = get_messages_from_device_id(db, device_id)
+    return messages
